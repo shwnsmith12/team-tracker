3cc0a9c65b5caf3b2817a4607fdd3fe63fd85344
Shawn <shwnsmith12@gmail.com>
2022-12-13 09:25:02 -0600

    added the 'add_employee' function

--- server.js
+++ server.js
@@ -125,3 +125,65 @@ function add_role() {
         })
     })
 }
+
+function add_employee() {
+    let employees = [];
+    let roles = [];
+
+    connection.query(`SELECT * FROM role`, function (err, data) {
+        if (err) throw err;
+
+        for (let i = 0; i < data.length; i++) {
+            roles.push(data[i].title);
+        }
+
+    connection.query(`SELECT * FROM employee`, function (err, data) {
+        if (err) throw err;
+
+        for (let i = 0; i < data.length; i++) {
+            roles.push(data[i].first_name);
+        }
+
+    inquirer
+      .prompt([
+        {
+            name: 'first_name',
+            message: "What is the employee's first name?",
+            type: 'input'
+        },
+        {
+            name: 'last_name',
+            message: "What is the employee's last name?",
+            type: 'input'
+        },
+        {
+            name: 'role_id',
+            message: "What is the employee's role with the company?",
+            type: 'list',
+            choices: roles,
+        },
+        {
+            name: 'manager_id',
+            message: "Who is the employee's manager?",
+            type: 'list',
+            choices: ['none'].concat(employees)
+        }
+     ]).then(function ({ first_name, last_name, role_id, manager_id }) {
+        let queryText = `INSERT INTO employee (first_name, last_name, role_id`;
+        if (manager_id != 'none') {
+            queryText += `, manager_id) VALUES ('${first_name}', '${last_name}', ${roles.indexOf(role_id)}, ${employees.indexOf(manager_id) + 1})`
+        } else {
+            queryText += `) VALUES ('${first_name}', '${last_name}', ${roles.indexOf(role_id) + 1})`
+                    }
+                    console.log(queryText)
+
+                    connection.query(queryText, function (err, data) {
+                        if (err) throw err;
+
+                        getJob();
+                })
+            })
+
+        })
+    })
+}
